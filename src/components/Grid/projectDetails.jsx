829b8ce29d53da19c4454266a66d1eefc81ef49f
Harsh <harsh.bhumihar7@gmail.com>
2025-07-07 02:24:08 +0530

    added details

--- src/components/Grid/projectDetails.jsx
+++ src/components/Grid/projectDetails.jsx
@@ -3,7 +3,7 @@ export const projectDetails = [
     title: "HELPNEST",
     subtitle: "SERVICE PROVIDING & SEEKING MOBILE APP",
     coverImage:
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fazuki.6b42d3cf.jpg&w=1920&q=75",
+      "https://ik.imagekit.io/disast3r/helpnest/helpnest_logo_blue.png?updatedAt=1751831570909",
     description: [
       "Real-time platform connecting users with nearby service providers using live location, Mapbox API, and Firebase.",
       "Built with Flutter and BLoC architecture; integrated Firestore and Supabase for scalable data operations and secure file storage.",
@@ -12,16 +12,14 @@ export const projectDetails = [
     collaborators: ["Harsh Kumar"],
     duration: "4 months",
     images: [
-      "https://example.com/animee-1.jpg",
-      "https://example.com/animee-2.jpg",
-      "https://example.com/animee-3.jpg",
+      "https://ik.imagekit.io/disast3r/clean%20architecture/black.png?updatedAt=1751834727801",
     ],
   },
   {
     title: "BIZTRACK",
     subtitle: "BUSINESS MANAGEMENT APP",
     coverImage:
-      "https://joseph.cv/_next/image?url=%2Fproject-covers%2Fplane.jpg&w=1920&q=75",
+      "https://ik.imagekit.io/disast3r/biztrack/biztrack_logo_black.png?updatedAt=1751831490288",
     description: [
       "Business management app supporting inventory, orders, staff, and daily sales tracking in real time.",
       "Architected with modular BLoC structure and Firebase backend to ensure offline-first performance and multi-device sync.",
@@ -30,80 +28,80 @@ export const projectDetails = [
     collaborators: ["Harsh Kumar"],
     duration: "3 months",
     images: [
-      "https://ik.imagekit.io/disast3r/biZtrack-1/11.jpg?updatedAt=1748545172848",
+      "https://ik.imagekit.io/disast3r/biztrack/biztrack_logo_white.png?updatedAt=1751831490219",
       "https://ik.imagekit.io/disast3r/biZtrack-1/10.jpg?updatedAt=1748545172811",
       "https://ik.imagekit.io/disast3r/biZtrack-1/12.jpg?updatedAt=1748545172807",
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkanban-cropped.b511c014.jpg&w=1920&q=75",
+      "https://ik.imagekit.io/disast3r/biztrack/delivery_code.png?updatedAt=1751832655650",
     ],
   },
   {
     title: "GO EXTRA MILE",
     subtitle: "RIDE TRACKING & REWARDING APP",
     coverImage:
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnatural.63553f68.jpg&w=1920&q=75",
+      "https://ik.imagekit.io/disast3r/gem/gem_white_logo_4_5.png?updatedAt=1751831519989",
     description: [
       "Flash deal and reward platform offering users exclusive coupon codes and discounts across partner websites.",
       "Built using Flutter, Firebase, and Cloud Functions to manage referral logic, coupon distribution, and user segmentation in real time.",
     ],
-    role: "Frontend Lead",
-    collaborators: ["Natural AI Core Team"],
-    duration: "6 months",
+    role: "Core Flutter Developer",
+    collaborators: ["GEM DEV TEAM lead by Harsh"],
+    duration: "1 years, 3 months",
     images: [
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flist-assignee-cropped.23166e7d.jpg&w=1920&q=75",
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffilters.4ed82aeb.jpg&w=1920&q=75",
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftable-cropped.8a31461c.jpg&w=1920&q=75",
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkanban-cropped.b511c014.jpg&w=1920&q=75",
+      "https://ik.imagekit.io/disast3r/gem/gem_black_logo.png?updatedAt=1751831520030",
+      "https://ik.imagekit.io/disast3r/gem/HPLi_QrUbLp3I7Whv87DuvufQJ4cod5aPMUWPT_R9zjVNa84abUekOJWfs5ykhgNr8Z9w1052-h592-rw.jpg?updatedAt=1751832981915",
+      "https://ik.imagekit.io/disast3r/gem/R46LGaSb47ZG-I7w7m3RaluKLHwmUfsIRjVt3F5L17w8IcFbaE5maDQdE1aIAqqr_mEw1052-h592-rw.jpg?updatedAt=1751832981911",
     ],
   },
+  // {
+  //   title: "SKIFF",
+  //   subtitle: "EMAIL + DOCS",
+  //   coverImage:
+  //     "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fskiff.a96af1d4.jpg&w=1920&q=75",
+  //   description: [
+  //     "Skiff is a privacy-first email and document collaboration tool.",
+  //     "My contribution included creating reusable UI blocks and real-time sync logic for collaborative documents.",
+  //   ],
+  //   role: "UI Engineer",
+  //   collaborators: ["Skiff Labs"],
+  //   duration: "5 months",
+  //   images: [
+  //     "https://example.com/skiff-1.jpg",
+  //     "https://example.com/skiff-2.jpg",
+  //   ],
+  // },
   {
-    title: "SKIFF",
-    subtitle: "EMAIL + DOCS",
-    coverImage:
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fskiff.a96af1d4.jpg&w=1920&q=75",
-    description: [
-      "Skiff is a privacy-first email and document collaboration tool.",
-      "My contribution included creating reusable UI blocks and real-time sync logic for collaborative documents.",
-    ],
-    role: "UI Engineer",
-    collaborators: ["Skiff Labs"],
-    duration: "5 months",
-    images: [
-      "https://example.com/skiff-1.jpg",
-      "https://example.com/skiff-2.jpg",
-    ],
-  },
-  {
-    title: "AIGA SQUARE",
-    subtitle: "DESIGN GALLERY",
+    title: "CLEAN ARCHITECTURE",
+    subtitle: "DESIGN SYSTEM",
     coverImage:
       "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Faiga-square.1d7c2316.jpg&w=1920&q=75",
     description: [
-      "AIGA Square is a digital design showcase platform featuring works from around the globe.",
-      "I contributed to the gallery UI, filter animations, and smooth grid transitions.",
+      "Separation of Concerns: Clean Architecture divides code into distinct layers (e.g., Domain, Data, and Presentation), ensuring that business logic is independent of frameworks, UI, or external tools.",
+      "High Testability and Flexibility: By isolating core logic and using dependency inversion, it becomes easier to test, scale, and swap technologies (like databases or APIs) without affecting core functionality.",
     ],
-    role: "Frontend Developer",
-    collaborators: ["AIGA Collective"],
-    duration: "2 months",
+    role: "EASE IN DEVELOPMENT",
+    collaborators: ["HARSH"],
+    duration: "3 years",
     images: [
-      "https://example.com/aiga-1.jpg",
-      "https://example.com/aiga-2.jpg",
+      "https://ik.imagekit.io/disast3r/clean%20architecture/white.png?updatedAt=1751834727801",
+      "https://ik.imagekit.io/disast3r/clean%20architecture/black.png?updatedAt=1751834727801",
+      "https://ik.imagekit.io/disast3r/clean%20architecture/Screenshot%202025-07-07%20015032.png?updatedAt=1751833275386",
     ],
   },
-  {
-    title: "FORMAT BAR",
-    subtitle: "EDITOR PLUGIN",
-    coverImage:
-      "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fformat-bar.619d7fd6.jpg&w=1920&q=75",
-    description: [
-      "Format Bar is a lightweight plugin for text editors allowing quick formatting without breaking flow.",
-      "I built the plugin for multiple frameworks and implemented seamless accessibility.",
-    ],
-    role: "Plugin Engineer",
-    collaborators: ["Open Source Maintainers"],
-    duration: "1.5 months",
-    images: [
-      "https://example.com/format-1.jpg",
-      "https://example.com/format-2.jpg",
-    ],
-  },
+  // {
+  //   title: "FORMAT BAR",
+  //   subtitle: "EDITOR PLUGIN",
+  //   coverImage:
+  //     "https://joseph.cv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fformat-bar.619d7fd6.jpg&w=1920&q=75",
+  //   description: [
+  //     "Format Bar is a lightweight plugin for text editors allowing quick formatting without breaking flow.",
+  //     "I built the plugin for multiple frameworks and implemented seamless accessibility.",
+  //   ],
+  //   role: "Plugin Engineer",
+  //   collaborators: ["Open Source Maintainers"],
+  //   duration: "1.5 months",
+  //   images: [
+  //     "https://example.com/format-1.jpg",
+  //     "https://example.com/format-2.jpg",
+  //   ],
+  // },
 ];
