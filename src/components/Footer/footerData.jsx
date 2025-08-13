3f0c32b55c0d66ebc1b6b76cdfa90c8690d416ea
Harsh <harsh.bhumihar7@gmail.com>
2025-08-13 21:51:49 +0530

    Resume updated with v3

--- src/components/Footer/footerData.jsx
+++ src/components/Footer/footerData.jsx
@@ -1,12 +1,12 @@
 const footerData = {
   version: {
-    number: "v0.0.2",
-    lastUpdated: "2025-07-04",
+    number: "v0.0.3",
+    lastUpdated: "2025-08-13",
   },
   socials: [
     {
       label: "Download Resume",
-      url: "https://drive.google.com/file/d/131AjRC7m7LVhWauCC4IBuQeC1awsD8HN/view?usp=sharing",
+      url: "https://drive.google.com/file/d/1RCvIEfidEWV7jdfoe1UyVy9OQwdTh8Kw/view?usp=sharing",
     },
     { label: "LinkedIn", url: "https://www.linkedin.com/in/harsh3134/" },
     { label: "Instagram", url: "https://instagram.com/ihxrxh" },
