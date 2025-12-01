f94006c770ce36c258d02128082baebe5fcaa8b9
Harsh <harsh.bhumihar7@gmail.com>
2025-12-01 21:45:48 +0530

    worked on about page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,34 @@
 export default function Home() {
+  const h1Style = `text-5xl py-7 max-w-[500px] text-center`
+  const buttonStyle = `cursor-pointer py-3 px-7 bg-gray-100 rounded-full hover:bg-gray-200`
   return (
-    <div>
-      <span>Hi</span>
+    <div className="py-25 flex flex-col items-center">
+      <p className="font-normal">Harsh Kumar</p>
+      <h1 className={h1Style}>
+        Aspiring <span className="underline text-gray-500">Backend Developer</span> - Typescript
+      </h1>
+      <p className="font-normal max-w-[700px] text-center text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima mollitia qui, dolor quasi, deleniti nihil ex culpa quos necessitatibus totam error, optio aliquid vel inventore.</p>
+      <div className={"flex gap-7 py-10"}>
+        <button className={buttonStyle} >
+          <span>{"View Projects"}</span>
+        </button>
+        <button className={`${buttonStyle} bg-white`}>
+          <span>{"Contact me"}</span>
+        </button>
+      </div>
+      <img
+        src="https://downloads.ctfassets.net/kftzwdyauwt9/4FnPiaiJdmc3R04H7kGR4N/8b115455fddf2921431f454f65b6ee22/Research_Hero.png?w=1920&q=90&fm=webp"
+        alt="hero"
+        height={100}
+        className="rounded-lg w-auto px-30 my-10 object-cover"
+      />
+      <span className="mt-20">Flutter Developer - Intern</span>
+      <h1 className="text-xl mt-6">Go Extra Mile, Hyderabad</h1>
+      <h1 className="text-xl text-gray-500">May, 2024 - July, 2025</h1>
+      <p className="py-10 text-center max-w-[600px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero perferendis earum deleniti corrupti deserunt sed perspiciatis modi minus, reiciendis qui molestiae esse assumenda iusto quae nam placeat odio beatae quas eos! Mollitia amet autem voluptatum, iure esse dolorem eaque quae nisi eos! Explicabo culpa illum sed fuga laboriosam facere aliquam!</p>
+      <span className="mt-20">Education</span>
+      <p className="text-xl mt-6">CSE, Bihar Engineering University</p>
+      <p className="text-xl text-gray-500">2019-2021</p>
     </div>
   );
 }
---
+++ apps/web/components/navbar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation"
+
+export function Navbar() {
+    const path = usePathname()
+    const drawerStyle = `flex-[1.5] h-screen flex flex-col justify-center px-6 space-y-2`
+    const drawerTyleStyle = `px-3 py-2 hover:bg-gray-100 transition rounded`
+
+    const tyleStyle = (link: string) => {
+        return link == path ? `${drawerTyleStyle} bg-gray-100` : drawerTyleStyle
+    }
+
+    return (
+        <section className={drawerStyle}>
+            <a href="/" className={tyleStyle("/")}>About</a>
+            <a href="/projects" className={tyleStyle("/projects")}>Projects</a>
+        </section>
+    )
+}
