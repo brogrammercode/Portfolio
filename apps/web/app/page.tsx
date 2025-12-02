6bf39048ad33641f06b0d273a45019237a3f3712
Harsh <harsh.bhumihar7@gmail.com>
2025-12-02 11:05:42 +0530

    about section implemented

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,28 @@
+import { ArrowRightIcon } from "lucide-react";
+
 export default function Home() {
   const h1Style = `text-5xl py-7 max-w-[500px] text-center`
-  const buttonStyle = `cursor-pointer py-3 px-7 bg-gray-100 rounded-full hover:bg-gray-200`
+  const buttonStyle = `cursor-pointer py-2 px-7 bg-gray-100 rounded-full hover:bg-gray-200`
+
+  const goextramileTasks = [
+    "Delivered 14+ production-ready modules, boosting app functionality and increasing DAU by 15%.",
+    "Improved reliability by cutting crash rates by 60% through anti-redundancy and stabilization practices.",
+    "Mentored 5+ junior developers, increasing team code contribution by 25% and reducing bug density.",
+    "Led the adoption of Clean Architecture, reducing feature delivery time by 30% and improving onboarding efficiency.",
+    "Integrated Razorpay and Google Maps APIs, driving a 20% increase in engagement for enabled features.",
+    "Collaborated with product teams to design a scalable backend solution supporting a 10× projected user load."
+  ];
+
+
   return (
     <div className="py-25 flex flex-col items-center">
-      <p>Harsh Kumar</p>
+      <p className="text-lg">Harsh Kumar</p>
       <h1 className={h1Style}>
-        Aspiring <span className="underline text-gray-500">Backend Developer</span> - Typescript
+        Backend Developer
       </h1>
-      <p className="font-normal max-w-[700px] text-center text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima mollitia qui, dolor quasi, deleniti nihil ex culpa quos necessitatibus totam error, optio aliquid vel inventore.</p>
+      <span className="font-normal max-w-[700px] text-center text-lg">
+        Aspiring <span className="font-semibold">Backend Developer</span> skilled in <span className="underline">TypeScript</span>, applying <span className="underline">best practices</span> and industry-standard tools to create efficient and scalable product solutions.
+      </span>
       <div className={"flex gap-1 py-10"}>
         <button className={buttonStyle} >
           <span>{"View Projects"}</span>
@@ -25,7 +40,14 @@ export default function Home() {
       <span className="mt-20">Flutter Developer - Intern</span>
       <h1 className="text-xl mt-6">Go Extra Mile, Hyderabad</h1>
       <h1 className="text-xl text-gray-500">May, 2024 - July, 2025</h1>
-      <p className="py-10 text-center max-w-[600px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero perferendis earum deleniti corrupti deserunt sed perspiciatis modi minus, reiciendis qui molestiae esse assumenda iusto quae nam placeat odio beatae quas eos! Mollitia amet autem voluptatum, iure esse dolorem eaque quae nisi eos! Explicabo culpa illum sed fuga laboriosam facere aliquam!</p>
+      <div className="py-10 max-w-[600px]">
+        {goextramileTasks.map((task, i) => (
+          <div key={i} className="flex items-center">
+            <ArrowRightIcon size={17} />
+            <p className="ml-4 py-0.5">{task}</p>
+          </div>
+        ))}
+      </div>
       <span className="mt-20">Education</span>
       <p className="text-xl mt-6">CSE, Bihar Engineering University</p>
       <p className="text-xl text-gray-500">2019-2021</p>
--- apps/web/app/projects/page.tsx
+++ apps/web/app/projects/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="text-xl">
+        <div>
             Projects
         </div>
     );
